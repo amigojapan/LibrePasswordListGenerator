c3ea8201312710abed3e243b4ab57d4aad325f57
Usmar A Padow <usmpadow@gmail.com>
2016-07-17 19:11:41 +0900

    ok, this one is semi usable

--- LibrePasswordListGenerator.py
+++ LibrePasswordListGenerator.py
@@ -6,11 +6,13 @@ import json
 import sys
 
 root = tk.Tk()
+class public_globals():
+	FileNameWidget=None
+	MasterPassword="unset"
+	PasswordLength=9
+	FileName="default_sites_list.json"
+	gridarray = []
 
-MasterPassword="unset"
-PasswordLength=9
-FileName="default_sites_list.json"
-gridaray = []
 class PasswordRow():
 	PasswordWidget=None
 	PasswordLabel=None
@@ -18,107 +20,109 @@ class PasswordRow():
 	SiteLabel=None
 	DeleteButton=None
 	
-def id_generator(size=PasswordLength, chars=string.ascii_uppercase + string.ascii_lowercase+string.digits +  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~' ):
+def id_generator(size=public_globals.PasswordLength, chars=string.ascii_uppercase + string.ascii_lowercase+string.digits +  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~' ):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 def callback_delete_password_ok(index):
 	print index
-	gridaray[index].SiteWidget.insert(0, "DELETED--")
-	
-def callback_add_password_ok():
+	public_globals.gridarray[index].SiteWidget.insert(0, "DELETED--")
+
+def add_row(site):
 	row = PasswordRow()
 	row.PasswordWidget = tk.Entry(root)
 	row.PasswordWidget.insert(0, id_generator())
-	row.PasswordLabel = tk.Label(root, text="Password #" + str(len(gridaray)+1) + ":")
+	row.PasswordLabel = tk.Label(root, text="Password #" + str(len(public_globals.gridarray)+1) + ":")
 	row.SiteWidget = tk.Entry(root)
-	row.SiteWidget.insert(0, "http://")
-	row.SiteLabel = tk.Label(root, text="site #" + str(len(gridaray)+1) + ":")
-	index=len(gridaray)
+	row.SiteWidget.insert(0, site)
+	row.SiteLabel = tk.Label(root, text="site #" + str(len(public_globals.gridarray)+1) + ":")
+	index=len(public_globals.gridarray)
 	row.DeleteButton = tk.Button(root, text="Delete", command=lambda:callback_delete_password_ok(index).pack())
-	row.PasswordLabel.grid(row=1+len(gridaray)+1,  column=0)
-	row.PasswordWidget.grid(row=1+len(gridaray)+1, column=1)
-	row.SiteLabel.grid(row=1+len(gridaray)+1,      column=2)
-	row.SiteWidget.grid(row=1+len(gridaray)+1,     column=3)
-	row.DeleteButton.grid(row=1+len(gridaray)+1,   column=4) 
-	#print len(gridaray)
-	gridaray.append(row)
+	row.PasswordLabel.grid(row=1+len(public_globals.gridarray)+1,  column=0)
+	row.PasswordWidget.grid(row=1+len(public_globals.gridarray)+1, column=1)
+	row.SiteLabel.grid(row=1+len(public_globals.gridarray)+1,      column=2)
+	row.SiteWidget.grid(row=1+len(public_globals.gridarray)+1,     column=3)
+	row.DeleteButton.grid(row=1+len(public_globals.gridarray)+1,   column=4) 
+	#print len(public_globals.gridarray)
+	public_globals.gridarray.append(row)
+	
+def callback_add_password_ok():
+	add_row("http://")
 
 def callback_save():
 	l=[]
-	for w in gridaray:
+	for w in public_globals.gridarray:
 		l.append(w.SiteWidget.get())
 	json_values = {
-    'PasswordLength': PasswordLength,
+    'PasswordLength': public_globals.PasswordLength,
     'sites': l
 	}
 	print(json.dumps(json_values))
 	json_string=json.dumps(json_values)
 	print dir(public_globals.FileNameWidget)
-	FileName=public_globals.FileNameWidget.get()
+	public_globals.FileName=public_globals.FileNameWidget.get()
 	
-	result = tkMessageBox.askquestion("Save sites list", "Are You Sure you want to save the file " + FileName + " and overwrite it?", icon='warning')
+	result = tkMessageBox.askquestion("Save sites list", "Are You Sure you want to save the file " + public_globals.FileName + " and overwrite it?", icon='warning')
 	if result == 'yes':
 		print "Saving..."
-		file_ = open(FileName, 'w')
+		file_ = open(public_globals.FileName, 'w')
 		file_.write(json_string)
 		file_.close()
 	else:
 		print "I'm Not Saving Yet.."	
 	
 def callback_load():
-	FileName_TMP = tkFileDialog.askopenfile(mode='w')
+	FileName_TMP = tkFileDialog.askopenfile(mode='r')
 	if not FileName_TMP==None: #filename will be None when no file is selected	
+		random.seed(public_globals.MasterPassword)
 		print FileName_TMP.name
-		file = open(FileName_TMP.name, 'r')
-		input_data=file.read()
-		file.close()
+		file_ = open(FileName_TMP.name, 'r')
+		input_data=file_.read()
+		file_.close()
 		print input_data
 		#delete all the UI for the previeus entries
-		#for w in gridaray:
-			#w.PasswordWidget.destroy()
-			#w.PasswordLabel.destroy()
-			#w.SiteWidget.destroy()
-			#w.SiteLabel.destroy()
-			#w.DeleteButton.destroy()
-		#gridaray=[]
+		for w in public_globals.gridarray:
+			w.PasswordWidget.destroy()
+			w.PasswordLabel.destroy()
+			w.SiteWidget.destroy()
+			w.SiteLabel.destroy()
+			w.DeleteButton.destroy()
+		public_globals.gridarray=[]
 		try:
 			parsed_json = json.loads(input_data)
 		except: # catch *all* exceptions
 			e = sys.exc_info()[0]
 			tkMessageBox.showinfo("Error!", "Error: %s, if this error is value error, you are either opening an empty file or an invalid file" % e)
-		PasswordLength = parsed_json['PasswordLength']
+		public_globals.PasswordLength = parsed_json['PasswordLength']
 		SitesList = parsed_json['sites']
 		for site in SitesList:
-				print sire
-
-class public_globals():
-	FileNameWidget=None
+			print site
+			add_row(site)
 def callback_master_password_ok():
-	MasterPassword = e.get()
-	random.seed(MasterPassword)
+	public_globals.MasterPassword = e.get()
+	random.seed(public_globals.MasterPassword)
 	l.destroy()
 	e.destroy()
 	b.destroy()
 	MasterPasswordWidget = tk.Entry(root)
-	MasterPasswordWidget.insert(0, MasterPassword)
+	MasterPasswordWidget.insert(0, public_globals.MasterPassword)
 	MasterPasswordLabel = tk.Label(root, text="Master Password:")
 	PasswordLengthWidget = tk.Entry(root)
-	PasswordLengthWidget.insert(0, PasswordLength)
+	PasswordLengthWidget.insert(0, public_globals.PasswordLength)
 	PasswordLengthLabel = tk.Label(root, text="Password Length:")
 	AddPasswordButton = tk.Button(root, text="Add Password", command=callback_add_password_ok)
 	SaveButton = tk.Button(root, text="Save site lables as file", command=callback_save)
 	LoadButton = tk.Button(root, text="Load site labels from file", command=callback_load)
 	public_globals.FileNameWidget = tk.Entry(root)
-	public_globals.FileNameWidget.insert(0, FileName)
+	public_globals.FileNameWidget.insert(0, public_globals.FileName)
 	FileNameLabel = tk.Label(root, text="File Name:")
 	MasterPasswordLabel.grid(row=0,column=0)
 	MasterPasswordWidget.grid(row=0,column=1)
 	PasswordLengthLabel.grid(row=0,column=2)
 	PasswordLengthWidget.grid(row=0,column=3)
-	AddPasswordButton.grid(row=1,  column=0)
-	SaveButton.grid(row=1,         column=1)
-	LoadButton.grid(row=1,         column=2)
-	FileNameLabel.grid(row=1,      column=3)
+	AddPasswordButton.grid(row=1,                 column=0)
+	SaveButton.grid(row=1,                        column=1)
+	LoadButton.grid(row=1,                        column=2)
+	FileNameLabel.grid(row=1,                     column=3)
 	public_globals.FileNameWidget.grid(row=1,     column=4)
 	
 e = tk.Entry(root)
@@ -128,3 +132,6 @@ l.grid(row=0,column=0)
 e.grid(row=1,column=0)
 b.grid(row=2,column=0)
 root.mainloop()
+#TO-DO:
+#make password length changable by GUI
+#warn that changing password length will change all the generated password 
